88f45b6dffff65cffd74f44dde336fcb1d4469dc
Ralfik <developeraul@gmail.com>
2021-07-20 23:18:14 +0500

    начало стилизование инпута на квазаре

--- src/pages/Begin.vue
+++ src/pages/Begin.vue
@@ -5,6 +5,23 @@
       src="begin"
     />
   <Button text="Отрпавить"/>
+  <label for="">
+    Ваш телефон
+  </label>
+  <q-input
+    outlined
+    dark
+    color="white"
+    bg-color="white"
+    
+    dense
+    :style="[{boxShadow: 'rgba(0, 0, 0, .15) 0px 0px 12px 1px'},{borderRadius: '6px'}]"
+    
+  > 
+     <template v-slot:prepend>
+    <Icon name="insta" class="tw-inline"/>
+     </template>
+  </q-input>
   
 
   </q-page>
