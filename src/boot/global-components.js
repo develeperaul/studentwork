d1f1f9ca43eae44b51c874e3da32fd318abd5e37
Ralfik <developeraul@gmail.com>
2021-07-20 22:20:44 +0500

    установил валидатор

--- src/boot/global-components.js
+++ src/boot/global-components.js
@@ -1,4 +1,24 @@
 import  Vue from 'vue';
+import { ValidationProvider, ValidationObserver } from 'vee-validate';
+
+
+import { extend, localize } from 'vee-validate';
+import ru from 'vee-validate/dist/locale/ru';
+import * as rules from 'vee-validate/dist/rules';
+
+localize('ru', ru);
+
+const needRules = [
+  'required',
+  'ext',
+  'size',
+  'min',
+  'length',
+];
+
+needRules.forEach((rule) => {
+  extend(rule, rules[rule]);
+});
 
 Vue.component('Icon', require('src/components/icon/Icon.vue').default);
 
