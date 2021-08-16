505a92163c944662c9f1c5bad7304b0f7d1fa716
nikita <nikita45454@gmail.com>
2021-08-16 18:31:02 +0500

    fix: исправляет скролл для ios

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,37 +1,19 @@
 <template>
-  <q-layout
-    view="hhh lpr FFF"
-    container
-    style="height:100vh"
-  >
+  <q-layout view="hhh lpr FFF">
     <q-footer
-      class="tw-bg-white  tw-overflow-hidden tw-pt-1.5 tw-pb-3.5 tw-z-10"
-      style="box-shadow: 0px 0px 18px 0px #E5E5E5; padding-top: 6px; padding-bottom: 14px;
-"
+      class="tw-bg-white tw-overflow-hidden tw-pt-1.5 tw-pb-3.5 tw-z-10"
+      style="
+        box-shadow: 0px 0px 18px 0px #e5e5e5;
+        padding-top: 6px;
+        padding-bottom: 14px;
+      "
     >
       <div class="-m-content">
-        <q-toolbar class="tw-grid tw-grid-flow-col  ">
-          <NavLink
-            name="about"
-            src="about"
-            title="О нас"
-            :exact="true"
-          />
-          <NavLink
-            name="training"
-            src="training"
-            title="Обучение"
-          />
-          <NavLink
-            name="rate"
-            src="rate"
-            title="Тарифы"
-          />
-          <NavLink
-            name="begin"
-            src="begin"
-            title="Начать"
-          />
+        <q-toolbar class="tw-grid tw-grid-flow-col">
+          <NavLink name="about" src="about" title="О нас" :exact="true" />
+          <NavLink name="training" src="training" title="Обучение" />
+          <NavLink name="rate" src="rate" title="Тарифы" />
+          <NavLink name="begin" src="begin" title="Начать" />
         </q-toolbar>
       </div>
     </q-footer>
@@ -39,32 +21,22 @@
       <router-view class="p-content" />
     </q-page-container>
   </q-layout>
-
 </template>
 
 <script>
-
-import NavLink from "components/NavLink"
-
-
-
-
+import NavLink from 'components/NavLink';
 
 export default {
   components: {
-    NavLink
+    NavLink,
   },
   name: 'MainLayout',
 
-  data () {
-    return {
-
-    }
+  data() {
+    return {};
   },
-  methods: {
-
-  }
-}
+  methods: {},
+};
 </script>
 
 <style scoped>
